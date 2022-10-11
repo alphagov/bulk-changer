2af078253bd06fb8bb2ae020d04c09a143f80396
James Robinson <james.robinson@digital.cabinet-office.gov.uk>
2022-10-11 13:42:00 +0100

    Update PR description

--- lib/pr_raiser.rb
+++ lib/pr_raiser.rb
@@ -4,7 +4,15 @@ require "open-uri"
 class PrRaiser
   BRANCH_NAME = "sync-dependabot-prs"
   COMMIT_TITLE = "Create workflow to copy PR template onto Dependabot PRs"
-  PR_DESCRIPTION = "This PR has been automatically raised by a script. For more details, please visit https://github.com/alphagov/dependabot-pr-sync"
+  PR_DESCRIPTION = <<~EOF
+    This repo contains a pull request template. However, PRs raised by Dependabot will not use this template. This could mean that PR reviewers might miss crucial information about the test suite, or about any manual steps that must be performed pre- or post- merge.
+
+    This PR adds a GitHub Actions workflow that will post the PR template as a comment on every PR that Dependabot raises.
+    
+    ---
+    
+    <sup>🤖 This PR was automatically raised by a script. For more details, please visit https://github.com/alphagov/dependabot-pr-sync or ask in the [Platform Reliability Slack channel](https://gds.slack.com/archives/CAEDZ4A8N).</sup>
+  EOF
 
   def raise_prs!
     repos.each do |repo|
