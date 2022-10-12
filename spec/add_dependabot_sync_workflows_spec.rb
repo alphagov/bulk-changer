25c2c5d742a48af6797374aa09c983d4e902f197
James Robinson <james.robinson@digital.cabinet-office.gov.uk>
2022-10-12 18:24:36 +0100

    Make the spec more RSpec-y

--- spec/add_dependabot_sync_workflows_spec.rb
+++ spec/add_dependabot_sync_workflows_spec.rb
@@ -1,7 +1,7 @@
 require "add_dependabot_sync_workflows"
 
 describe "#add_dependabot_sync_workflows!" do
-  it "raises a PR for repos that have a PR template" do
+  before do
     stub_govuk_repos([
       "repo-without-pr-template",
       "repo-with-pr-template-but-no-sync-workflow",
@@ -13,19 +13,23 @@ describe "#add_dependabot_sync_workflows!" do
     stub_github_repo("repo-with-pr-template-but-no-sync-workflow", contents: [".github/pull_request_template.md"])
     stub_github_repo("repo-with-pr-template-and-pr-to-create-sync-workflow", contents: [".github/pull_request_template.md"], feature_branches: [BRANCH])
     stub_github_repo("repo-with-sync-workflow", contents: [".github/pull_request_template.md", ".github/workflows/copy-pr-template-to-dependabot-prs.yaml"])
+  end
     
-    create_branch_stub = stub_create_branch_request("repo-with-pr-template-but-no-sync-workflow", BRANCH)
+  let!(:create_branch_stub) { stub_create_branch_request("repo-with-pr-template-but-no-sync-workflow", BRANCH) }
 
-    create_contents_stub = stub_create_contents_request(
+  let!(:create_contents_stub) {
+    stub_create_contents_request(
       "repo-with-pr-template-but-no-sync-workflow",
       src_path: "copy-pr-template-to-dependabot-prs.yaml",
       dst_path: ".github/workflows/copy-pr-template-to-dependabot-prs.yaml",
       commit_title: TITLE,
       branch: BRANCH
     )
+  }
     
-    raise_pr_stub = stub_create_pull_request("repo-with-pr-template-but-no-sync-workflow", branch: BRANCH, title: TITLE, description: DESCRIPTION)
+  let!(:raise_pr_stub) { stub_create_pull_request("repo-with-pr-template-but-no-sync-workflow", branch: BRANCH, title: TITLE, description: DESCRIPTION) }
 
+  it "raises a PR for repos that have a PR template" do
     expect { add_dependabot_sync_workflows! }.to output("Raising PR for repo-with-pr-template-but-no-sync-workflow... ✅\n").to_stdout
 
     expect(create_branch_stub).to have_been_requested
